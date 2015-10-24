8f557b058a828cf69bdb514a5bd25cf959377e88
polano <pedro_olano@hotmail.com>
2015-10-24 11:56:37 -0500

    ok
    ok

---
+++ modulo4/lab8/controllers/router.js
@@ -0,0 +1,13 @@
+var express = require('express');
+
+var router = express.Router();
+
+router.get('/', function(req, res){
+	res.render('index', {'title' : 'Lab7'});
+});
+
+router.get('/contact', function(req, res){
+	res.render('contact', {'title' : 'Lab8'});
+});
+
+module.exports = router;
